789b72cf9d5231f127e1cc43581a73b4941fe28a
u171799 <u171799@s001-n141.aidevcloud>
2022-11-27 10:04:27 -0800

    Se hace la parte de Task del ejemplo

--- src/lab2/Tareas/fibo_task.c
+++ src/lab2/Tareas/fibo_task.c
@@ -1,3 +1,6 @@
+/* To compile Classic Compiler: icc -o fibo_task.icc fibo_task.c -qopenmp -qopt-report */
+/*                               more fibo_task.optrpt */
+/*                               ./fibo_task.icc 2 10 */
 #include <stdio.h>
 #include <omp.h>
 
@@ -7,11 +10,11 @@ long comp_fib_numbers(int n)
 	if ( n == 0 || n == 1 ) return(1);
 	if ( n<20 ) return(comp_fib_numbers(n-1) +comp_fib_numbers(n-2));
 
-	#pragma omp task ...
+	#pragma omp task shared(fnm1)
 	{fnm1 = comp_fib_numbers(n-1);}
-	#pragma omp task ...
+	#pragma omp task shared(fnm2)
 	{fnm2 = comp_fib_numbers(n-2);}
-	#pragma omp ...
+	#pragma omp taskwait
 	fn = fnm1 + fnm2;
 
 	return(fn);
@@ -25,14 +28,15 @@ int main(int argc, char* argv[]) {
 	if (argc != 3) {
 		printf("usage:  ./exec <thread count> <number of Fibonacci numbers>\n");
 	}
-	thread_count = atoi(argv[1]); omp_set_num_threads(thread_count);
+	thread_count = atoi(argv[1]); 
+	omp_set_num_threads(thread_count);
 	n = atoi(argv[2]);
 
 	double start = omp_get_wtime();
 
-#pragma omp parallel
+#pragma omp parallel shared(n)
 {
-	#pragma omp single 
+	#pragma omp single
 	fibo = comp_fib_numbers(n);
 }
 
