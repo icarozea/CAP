ac086078300efb6b2479bf52f5108f3009ac0d57
icarozea <ovidio.zea@gmail.com>
2022-11-29 18:25:53 +0100

    Update solver.c
    Leer rojas

--- src/lab2/RealFluidDynamicsGames/solver.c
+++ src/lab2/RealFluidDynamicsGames/solver.c
@@ -36,6 +36,18 @@ static void lin_solve(unsigned int n, boundary b, float * x, const float * x0, f
 
 
     for (unsigned int k = 0; k < 20; k++) {
+	    
+	for (unsigned int i = 1; i <= n; i++) {
+            for (unsigned int j = 1; j <= n; j++) {
+		if((i%2 == 0 && j%2!=0) || (i%2 != 0 && j%2==0)){    
+			x[IX(i, j)] = (x0[IX(i, j)] + a * (x[IX(i - 1, j)] +
+							   x[IX(i + 1, j)] +
+							   x[IX(i, j - 1)] +
+							   x[IX(i, j + 1)])) / c;
+		}
+            }
+        }
+	    
 	for (unsigned int i = 1; i <= n; i++) {
             for (unsigned int j = 1; j <= n; j++) {
                 x[IX(i, j)] = (x0[IX(i, j)] + a * (x[IX(i - 1, j)] +
@@ -43,7 +55,7 @@ static void lin_solve(unsigned int n, boundary b, float * x, const float * x0, f
                                                    x[IX(i, j - 1)] +
                                                    x[IX(i, j + 1)])) / c;
             }
-        }
+        }    
         set_bnd(n, b, x);
     }
 }
